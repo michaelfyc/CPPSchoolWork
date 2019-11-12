6338287a1ed64cde208513b609ad143975bbf9d2
michaelfyc <45136049+michaelfyc@users.noreply.github.com>
2019-11-12 17:44:04 +0800

    Update Main.cpp
    Wrong Result: user's deposit won't change by calling bank.save(user.value)
    Debug tomorrow.

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,7 @@ int main()
     std::cout<<"\n";
     Bank bank;
     User user=bank.createAccount();
-    user.save(100.23);
     bank.save(user,120);
-    user.getInfo();
     bank.showStatus(user);
     return 0;
 }
